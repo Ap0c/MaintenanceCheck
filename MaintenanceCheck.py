61c03aba31fdd7e5202b775eb96b4e40fc76f51c
Ap0c <Ap0c@users.noreply.github.com>
2013-09-13 17:11:07 +0100

    Neatening and Usability
    Added a couple of printouts to make sure the user does not quit while
    the scripts are running, and made how to use the program more obvious.

--- MaintenanceCheck.py
+++ MaintenanceCheck.py
@@ -7,31 +7,38 @@ secondsinday = 86400
 secondsinweek = 604800
 secondsinmonth = 2629743
 
-print "Running maintenance, do not close the window..."
+def printrunning():
+	print "Running maintenance, please enter your password and do not close the window once you have entered it. The program will inform you when it is done."
 
 #Checks to see when the scripts were last run and, if it's been a while, runs them.
 try:
 	dailytime = os.path.getmtime("/var/log/daily.out") #Checks when the daily script output file was last modified.
 	sincedaily = time.time() - dailytime
 	if sincedaily > secondsinday: #If it was last modified more than a day ago, runs the script.
+		printrunning()
 		os.system("sudo periodic daily")
 except OSError: #If the file does not exist, it's possible the script has never been run, this runs it.
+	printrunning()
 	os.system("sudo periodic daily")
 
 try:
 	weeklytime = os.path.getmtime("/var/log/weekly.out") #Checks when the weekly script output file was last modified.
 	sinceweekly = time.time() - weeklytime
 	if sinceweekly > secondsinweek: #If it was last modified more than a week ago, runs the script.
+		printrunning()
 		os.system("sudo periodic weekly")
 except OSError: #If the file does not exist, it's possible the script has never been run, this runs it.
+	printrunning()
 	os.system("sudo periodic weekly")
 
 try:
 	monthlytime = os.path.getmtime("/var/log/monthly.out") #Checks when the monthly script output file was last modified.
 	sincemonthly = time.time() - monthlytime
 	if sincemonthly > secondsinmonth: #If it was last modified more than a month ago, runs the script.
+		printrunning()
 		os.system("sudo periodic monthly")
 except OSError: #If the file does not exist, it's possible the script has never been run, this runs it.
+	printrunning()
 	os.system("sudo periodic monthly")
 
 print "Maintenance finished, you may now close the window."
